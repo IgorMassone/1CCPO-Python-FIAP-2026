74b526a2e7a0c6fc0958232dcf351b5c4a831616
IgorMassone <igormassone06@gmail.com>
2026-03-19 20:57:11 -0300

    Aula 2: Variáveis e Operadores

---
+++ aula02-variaveis-operadores/aula02a-exercicios.py
@@ -0,0 +1,14 @@
+nome = input("Qual é o seu nome: ")
+
+print(f"Seja bem-vindo {nome}")
+
+dia = int(input("Qual é o seu dia de nascimento: "))
+mes = int(input("Qual é o seu mês de nascimento: "))
+ano = int(input("Qual é o seu ano de nascimento: "))
+
+print(f"A sua data de nascimento é: {dia}/{mes}/{ano}")
+
+nm1 = int(input("Digite o primeiro número: "))
+nm2 = int(input("Digite o segundo número: "))
+
+print(f"A soma entre eles é: {nm1+ nm2}")
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,36 @@
+print("Olá mundo")
+
+print(7+4)
+print("7 + 4")
+print("7" + "4") # Concatenação de strings
+
+# Comentários de 1 linha
+'''
+    Comentários de
+    múltiplas
+    linhas
+    Autor: Igor Massone
+    Versão: 1.0.1
+'''
+
+# Variáveis
+nome = "Igor" #str
+idade = 17 #int
+peso = 58.2 #float
+
+print(nome, idade, peso)
+print(f"Oiiiii, {nome}!!!")
+
+# INPUT - SIMULAÇÃO DE FORMULÁRIOS NO CMD
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+peso = float(input("Digite o seu peso: "))
+
+print(nome, idade, peso)
+print(idade + 1)
+
+ano_nascimento = 2008
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"A sua idade é: {idade}")
