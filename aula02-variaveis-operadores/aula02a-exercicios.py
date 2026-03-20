611d8d5b00e84a6e0224bb15953830e1b1eb99e9
IgorMassone <igormassone06@gmail.com>
2026-03-19 21:00:45 -0300

    Aula 02-variaveis (corrigindo erros)

--- aula02-variaveis-operadores/aula02a-exercicios.py
+++ aula02-variaveis-operadores/aula02a-exercicios.py
@@ -4,7 +4,7 @@ print(f"Seja bem-vindo {nome}")
 
 dia = int(input("Qual é o seu dia de nascimento: "))
 mes = int(input("Qual é o seu mês de nascimento: "))
-ano = int(input("Qual é o seu ano dse nascimento: "))
+ano = int(input("Qual é o seu ano de nascimento: "))
 
 print(f"A sua data de nascimento é: {dia}/{mes}/{ano}")
 
