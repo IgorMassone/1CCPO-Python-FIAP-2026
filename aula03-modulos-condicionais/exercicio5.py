3414f38d5d965325fa4502e5ef89f088546027b8
IgorMassone <igormassone06@gmail.com>
2026-04-12 00:01:30 -0300

    Exercícios propostos em sala

--- aula03-modulos-condicionais/exercicio5.py
+++ aula03-modulos-condicionais/exercicio5.py
@@ -1,2 +1,19 @@
 def ler_valores():
-    helloworld("print")s
+    n1 = int(input("Digite o primeiro valor: "))
+    n2 = int(input("Digite o segundo valor: "))
+    return n1, n2
+
+def calculo(n1,n2):
+    if n1 % n2 == 0:
+        print("São múltiplos")
+    elif n2 % n1 == 0:
+        print("São múltiplos")
+    else:
+        print("Não são múltiplos")
+
+def main():
+    n1, n2 = ler_valores()
+    calculo(n1,n2)
+
+if __name__ == "__main__":
+    main()
---
+++ aula03-modulos-condicionais/exercicio6.py
@@ -0,0 +1,9 @@
+def ler_numeros():
+    n1 = int(input("Digite o primeiro numero: "))
+    n2 = int(input("Digite o segundo numero: "))
+    operador = input("Digite o operador (+, -, *, /): ")
+    return n1,n2, operador
+
+def calculo(n1,n2,operador):
+    if operador == "+":
+        print(n1+n2)
