4a04234197e063031a4ebbfd3c7f84b0b38e6ae8
IgorMassone <igormassone06@gmail.com>
2026-04-12 22:46:48 -0300

    aula03 - exercicio6 calculadora

--- aula03-modulos-condicionais/exercicio6.py
+++ aula03-modulos-condicionais/exercicio6.py
@@ -5,5 +5,23 @@ def ler_numeros():
     return n1,n2, operador
 
 def calculo(n1,n2,operador):
-    if operador == "+":w
-        print(n1+n2)
+    if operador == "+":
+        return (n1+n2)
+    elif operador == "-":
+        return (n1-n2)
+    elif operador == "*":
+        return (n1*n2)
+    elif operador == "/":
+        if n2 == 0:
+            return "Erro: divisão por zero"
+        else:
+            return n1 / n2
+    
+def main():
+    n1, n2, operador = ler_numeros()
+    resultado = calculo(n1, n2, operador)
+    print(f"O resultado é: {resultado}")
+
+
+if __name__ == '__main__':
+    main()
