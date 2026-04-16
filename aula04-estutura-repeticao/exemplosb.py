aded5ee80e0009a5941078e5ab53aa7661118a54
IgorMassone <igormassone06@gmail.com>
2026-04-16 15:31:24 -0300

    Realizando desafios propostos em aula com funções e laços de repetição

---
+++ aula04-estutura-repeticao/exemplosb.py
@@ -0,0 +1,22 @@
+nota1 = float(input("Digite a primeira nota: "))
+while 0 <= nota1 <= 10:
+    print("Nota 1 armazenada")
+    break
+
+nota2 = float(input("Digite a segunda nota: "))
+while 0 <= nota2 <= 10:
+    print("Nota 2 armazenada")
+    break
+
+media = (nota1 + nota2) / 2
+
+print(f"A media será de: {media}")
+
+
+def receber_nota(a,b):
+    return (a+b) / 2
+
+print("A média é: " + receber_nota(5,10))
+
+
+
---
+++ aula04-estutura-repeticao/exemplosc.py
@@ -0,0 +1,20 @@
+def receber_notas(mensagem):
+    while True:
+        try:
+            nota = float(input(mensagem))
+            if not (0<=nota<=10):
+                print("Nota deve ser entre 0 e 10")
+                continue
+            return nota
+        except ValueError:
+            print("Digite um número válido")
+
+def calcular_media(nota1,nota2):
+    media = (nota1 + nota2) / 2
+    return media
+
+nota1 = receber_notas("Digite o primeiro número: ")
+nota2 = receber_notas("Digite o segundo número: ")
+media = calcular_media(nota1,nota2)
+
+print(f"A média das notas {nota1} e {nota2} é de: {media}")
