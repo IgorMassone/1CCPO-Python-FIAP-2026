8f1b09f38342fc45c23fdf4655650484ff8657d8
IgorMassone <igormassone06@gmail.com>
2026-04-23 20:15:54 -0300

    Correção de lógica no cálculo das notas

--- aula04-estutura-repeticao/exemplosb.py
+++ aula04-estutura-repeticao/exemplosb.py
@@ -1,12 +1,12 @@
 nota1 = float(input("Digite a primeira nota: "))
-while 0 <= nota1 <= 10:
-    print("Nota 1 armazenada")
-    break
+while nota1 < 0 or nota1 > 10:
+    print("A nota deve estar entre 0 e 10")
+    nota1 = float(input("Digite a nota novamente: "))
 
 nota2 = float(input("Digite a segunda nota: "))
-while 0 <= nota2 <= 10:
-    print("Nota 2 armazenada")
-    break
+while nota2 < 0 or nota2 > 10:
+    print("A nota deve estar entre 0 e 10")
+    nota2 = float(input("Digite a nota novamente"))
 
 media = (nota1 + nota2) / 2
 
