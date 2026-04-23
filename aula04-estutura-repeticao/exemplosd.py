9c56851965c90d8743d7757ebeea3266c1d13ea6
IgorMassone <igormassone06@gmail.com>
2026-04-23 20:40:57 -0300

    Adicionando função para validar número digitado pelo usuário

--- aula04-estutura-repeticao/exemplosd.py
+++ aula04-estutura-repeticao/exemplosd.py
@@ -1,4 +1,11 @@
-qtd = int(input("Digite a quantidade do produto: "))
+def validar_valor(valor):
+    while valor < 0:
+        print("Digite um valor positivo: ")
+        valor = int(input("Digite um novo valor:"))
+    return valor
 
-for i in range(qtd):
-    print("Produto")
+qtd = int(input("Digite a quantidade do produto: "))
+qtd = validar_valor(qtd)
+
+for i in range(1, qtd + 1):
+    print(f"{i}° Produto")
