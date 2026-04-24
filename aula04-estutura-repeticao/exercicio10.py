9ada784fa40eb045d84e348c8c5a529144ad5096
IgorMassone <igormassone06@gmail.com>
2026-04-24 19:33:16 -0300

    Exercício 10 - Pirâmide Invertida

---
+++ aula04-estutura-repeticao/exercicio10.py
@@ -0,0 +1,6 @@
+piramide = int(input("Digite o tamanho da pirâmide: "))
+
+for i in range(piramide,0,-1):
+    espacos = ' ' * (linhas - i)
+    asteriscos = '*' * (2 - i - 1)
+    print(espacos + asteriscos)
