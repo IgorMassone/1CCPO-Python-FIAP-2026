5c0b744c9dca09557e79c53a0688c3227e5fdeab
IgorMassone <igormassone06@gmail.com>
2026-08-13 20:45:33 -0300

    Contagem de Letras

---
+++ aula06-desafio-2.py
@@ -0,0 +1,10 @@
+def count_letters(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+print(count_letters("ageurbolox"))
