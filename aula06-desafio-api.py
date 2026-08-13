f27fa7cdb965c44a1f2db689a5a321c3dd9d8991
IgorMassone <igormassone06@gmail.com>
2026-08-13 19:45:21 -0300

    Criação da função de Sucesso e Falso

--- aula06-desafio-api.py
+++ aula06-desafio-api.py
@@ -5,5 +5,14 @@ status = [
 [201, 500, 502, 201, 500]
 ]
 
-print(endpoints[0])
-print(status[0])
+# print(endpoints[0])
+# print(status[0])
+
+# FUNÇÃO QUE VERIFICA SE UM CÓDIGO HTTP DA REQ. DE UM 
+# ENDPOINT É SUCESSO OU NÃO
+# 200 --> True
+# 4001 --> False
+
+def eh_sucesso(codigo):
+    return codigo >= 200 and codigo <= 299
+print(eh_sucesso(401))
